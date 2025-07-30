937769a3ff2b90946d3ce638995f63333e0c603e
小云同学 <shiinamashiro163@gmail.com>
2025-07-30 22:12:35 +0800

    LF

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,5 @@
-# /bin/bashdocker rm -f image-magician-frontenddocker rmi -f image-magician-frontenddocker build -f Dockerfile -t image-magician-frontend . --no-cachedocker run --name image-magician-frontend -p 10001:80 -d image-magician-frontend
+# /bin/bash
+docker rm -f image-magician-frontend
+docker rmi -f image-magician-frontend
+docker build -f Dockerfile -t image-magician-frontend . --no-cache
+docker run --name image-magician-frontend -p 10001:80 -d image-magician-frontend
